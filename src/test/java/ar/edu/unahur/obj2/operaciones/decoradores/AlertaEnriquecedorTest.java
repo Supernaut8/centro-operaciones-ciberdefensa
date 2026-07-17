fc520cb61c448fadc7416e17a249370479614d05
supernaut8 <gnroces@gmail.com>
2026-07-17 20:34:34 -0300

    commit

--- src/test/java/ar/edu/unahur/obj2/operaciones/decoradores/AlertaEnriquecedorTest.java
+++ src/test/java/ar/edu/unahur/obj2/operaciones/decoradores/AlertaEnriquecedorTest.java
@@ -4,9 +4,12 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 import ar.edu.unahur.obj2.operaciones.Alerta;
+import ar.edu.unahur.obj2.operaciones.Analista;
+import ar.edu.unahur.obj2.operaciones.estrategias.AnalistaNivel1;
 import ar.edu.unahur.obj2.operaciones.estrategias.EstrategiaDeEnriquecimiento;
 import ar.edu.unahur.obj2.operaciones.estrategias.Forense;
 import ar.edu.unahur.obj2.operaciones.estrategias.Rapido;
+import ar.edu.unahur.obj2.operaciones.excepciones.AnalistaSinCriterioException;
 import ar.edu.unahur.obj2.operaciones.excepciones.EnriquecimientoIncompatibleException;
 
 public class AlertaEnriquecedorTest {
@@ -24,7 +27,7 @@ public class AlertaEnriquecedorTest {
     @Test
     void analisisMalwareNoAceptaLocalhost() {
 
-        assertThrows(EnriquecimientoIncompatibleException.class,
+        assertThrows( EnriquecimientoIncompatibleException.class,
                 () -> new AnalisisMalware(
                         new Alerta("AL-001", "127.0.0.1", 20, 0)));
     }
@@ -50,6 +53,28 @@ public class AlertaEnriquecedorTest {
                 new Alerta("A", "1", 40, 0));
 
         assertEquals(60, alerta.getSeveridadTotal());
+
+    }
+
+    @Test
+    void puedeCambiarElCriterio() {
+
+        Analista analista = new Analista("123");
+
+        analista.setCriterio(new AnalistaNivel1());
+
+        assertNotNull(analista.getCriterio());
+
+    }
+
+    @Test
+    void lanzarExcepcionSiNoTieneCriterio() {
+
+        Analista analista = new Analista("123");
+
+        assertThrows(
+                AnalistaSinCriterioException.class,
+                analista::getCriterio);
 
     }
 }
